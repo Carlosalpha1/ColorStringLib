babe5fe4df4ea70fafe34d5e2bc0b2e8b7c32154
Carlos Caminero <carlos2caminero@gmail.com>
2021-07-13 15:07:35 +0200

    update ColorString.h style

--- ColorString.h
+++ ColorString.h
@@ -27,14 +27,15 @@ namespace ColorString
  * A list of all possible colors
  */
 enum {
-  Not_color, Black, Red,
-  Green, Brown, Orange,
-  Blue, Purple, Cyan,
-  Light_Gray, Dark_Gray,
-  Light_Red, Light_Green,
-  Yellow, Light_Blue,
-  Light_Purple, Light_Cyan,
-  White
+  Not_color,    Black,
+  Red,          Green,
+  Brown,        Orange,
+  Blue,         Purple,
+  Cyan,         Light_Gray,
+  Dark_Gray,    Light_Red,
+  Light_Green,  Yellow,
+  Light_Blue,   Light_Purple,
+  Light_Cyan,   White
 };
 
 
@@ -43,24 +44,24 @@ enum {
  * with a representation code
  */
 std::map<int, std::string> colors = {
-  {Not_color, "\033[0m"},
-  {Black, "\033[0;0m"},
-  {Red, "\033[0;31m"},
-  {Green, "\033[0;32m"},
-  {Brown, "\033[0;33m"},
-  {Orange, "\033[0;33m"},
-  {Blue, "\033[0;34m"},
-  {Purple, "\033[0;35m"},
-  {Cyan, "\033[0;36m"},
-  {Light_Gray, "\033[0;37m"},
-  {Dark_Gray, "\033[1;30m"},
-  {Light_Red, "\033[1;31m"},
-  {Light_Green, "\033[1;32m"},
-  {Yellow, "\033[1;33m"},
-  {Light_Blue, "\033[1;34m"},
-  {Light_Purple, "\033[1;35m"},
-  {Light_Cyan, "\033[1;36m"},
-  {White, "\033[1;37m"},
+  {Not_color,     "\033[0m"   },
+  {Black,         "\033[0;0m" },
+  {Red,           "\033[0;31m"},
+  {Green,         "\033[0;32m"},
+  {Brown,         "\033[0;33m"},
+  {Orange,        "\033[0;33m"},
+  {Blue,          "\033[0;34m"},
+  {Purple,        "\033[0;35m"},
+  {Cyan,          "\033[0;36m"},
+  {Light_Gray,    "\033[0;37m"},
+  {Dark_Gray,     "\033[1;30m"},
+  {Light_Red,     "\033[1;31m"},
+  {Light_Green,   "\033[1;32m"},
+  {Yellow,        "\033[1;33m"},
+  {Light_Blue,    "\033[1;34m"},
+  {Light_Purple,  "\033[1;35m"},
+  {Light_Cyan,    "\033[1;36m"},
+  {White,         "\033[1;37m"},
 };
 
 
@@ -72,7 +73,6 @@ std::map<int, std::string> colors = {
 template <int color_id = White>
 class String : public std::string
 {
-
 private:
   int color = color_id;
 
